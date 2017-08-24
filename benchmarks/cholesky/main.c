002240343eefa712e6852e0ed23c2999d3b0ff00
Jian Weng <jian.weng@ucla.edu>
2017-08-24 12:16:17 -0700

    small dfg applied, 2.56x speedup got!

--- benchmarks/cholesky/main.c
+++ benchmarks/cholesky/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <math.h>
 #include <stdlib.h>
+#include "compute.h"
+#include "../../common/include/sb_insts.h"
 #include "../../common/include/sim_timing.h"
 
 #define N 64
@@ -9,31 +12,68 @@
 float a[N * N], L[N * N];
 
 void cholesky(float *a, float *L) {
-  float *A = (float *) malloc(N * N * sizeof(float));
-  for (int i = 0; i < N * N; ++i) {
-    A[i] = a[i];
+  const int n = N + 2 | 1;
+  float *A = (float *) malloc(n * n * sizeof(float));
+
+  SB_CONFIG(compute_config, compute_size);
+
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      A[i * n + j] = a[i * N + j];
+    }
   }
   for (int i = 0; i < N; ++i) {
-    float div = sqrt(A[i * (N + 1)]);
-    float *b = A + i * (N + 1);
+    float div = sqrt(A[i * (n + 1)]);
+    //float *b = A + i * (n + 1);
+    float b[N - i + 1];
+    float bb[N - i];
     L[i * (N + 1)] = div;
     {
-      float *bj = b + 1;
+      float *_b = A + i * (n + 1);
+      float *bp = b;
+      *bp++ = *_b++;
+      float *bbp = bb;
       for (int j = i + 1; j < N; ++j) {
-        L[j * N + i] = *bj++ / div;
+        float v = *_b++;
+        *bp++ = v;
+        *bbp++= v;
+        L[j * N + i] = v / div;
       }
+      *bp = *bbp = 0;
     }
+    //begin_roi();
     {
       float *bj = b + 1, *bk;
-      float v = A[i * (N + 1)];
+      //float v = A[i * (n + 1)];
+      float v = 1. / A[i * (n + 1)];
+      char delta = 0;
       for (int j = i + 1; j < N; ++j) {
+
+        union {
+          float a[2];
+          uint64_t v;
+        } ri_bj = {*bj, *bj}, ri_v = {-v, -v};
+
+        int times = (N - j - 1 >> 1) + 1;
+        //times = 1;
+        //printf("%d,%d: %d\n", i, j, times);
+        delta ^= 1;
+        SB_CONST(P_compute_A, ri_bj.v, times);
+        SB_DMA_READ(delta ? bb + j - i - 1 : b + j - i, 8, 8, times, P_compute_B);
+        SB_CONST(P_compute_C, ri_v.v, times);
+        SB_DMA_READ(A + (n + 1) * j, 8, 8, times, P_compute_D);
+        SB_DMA_WRITE(P_compute_G, 8, 8, times, A + (n + 1) * j);
+
+        /*
         bk = b + j - i;
         for (int k = j; k < N; ++k) {
-          A[j * N + k] -= *bj * (*bk++) / v;
-        }
+          A[j * n + k] -= *bj * (*bk++) / v;
+        }*/
         ++bj;
       }
+      SB_WAIT_ALL();
     }
+    //end_roi();
   }
 }
 
