008e7f9c1150b8606822b28df5cbe7d0f79e9bce
Jian Weng <jian.weng@ucla.edu>
2017-09-25 11:42:26 -0700

    by hoisting all supported pattern outside the innermost loop, i get another 2x speedup. right now, 12.76x speedup is possible

--- MachSuite/fft/strided/fft_sb.c
+++ MachSuite/fft/strided/fft_sb.c
@@ -10,20 +10,17 @@ void fft(double real[FFT_SIZE], double img[FFT_SIZE], double real_twid[FFT_SIZE/
   SB_CONFIG(compute_config, compute_size);
 
   for(span=FFT_SIZE>>1; span; span>>=1, log++) {
+    int times = FFT_SIZE / span / 2;
+
+    SB_DMA_READ(real, 8 * span * 2, 8 * span, times, P_compute_EVE_REAL);
+    SB_DMA_READ(img, 8 * span * 2, 8 * span, times, P_compute_EVE_IMAG);
+
+    SB_DMA_READ(real + span, 8 * span * 2, 8 * span, times, P_compute_ODD_REAL);
+    SB_DMA_READ(img + span, 8 * span * 2, 8 * span, times, P_compute_ODD_IMAG);
+
     for (i = 0; i < FFT_SIZE; i += span + span) {
-      SB_DMA_READ(real + i, 8, 8, span, P_compute_EVE_REAL);
-      SB_DMA_READ(img + i, 8, 8, span, P_compute_EVE_IMAG);
-
-      SB_DMA_READ(real + i + span, 8, 8, span, P_compute_ODD_REAL);
-      SB_DMA_READ(img + i + span, 8, 8, span, P_compute_ODD_IMAG);
-
       SB_DMA_READ(real_twid, (1 << log) * 8, 8, span, P_compute_TWD_REAL);
       SB_DMA_READ(img_twid, (1 << log) * 8, 8, span, P_compute_TWD_IMAG);
-
-      SB_DMA_WRITE(P_compute_EVE_REAL_O, 8, 8, span, real + i);
-      SB_DMA_WRITE(P_compute_EVE_IMAG_O, 8, 8, span, img + i);
-      SB_DMA_WRITE(P_compute_ODD_REAL_O, 8, 8, span, real + i + span);
-      SB_DMA_WRITE(P_compute_ODD_IMAG_O, 8, 8, span, img + i + span);
 
       /*for (even = i, odd = i + span; even < i + span; ++even, ++odd) {
         temp = real[even] + real[odd];
@@ -41,6 +38,11 @@ void fft(double real[FFT_SIZE], double img[FFT_SIZE], double real_twid[FFT_SIZE/
         rootindex += 1 << log;
       }*/
     }
+    SB_DMA_WRITE(P_compute_EVE_REAL_O, 8 * span * 2, 8 * span, times, real);
+    SB_DMA_WRITE(P_compute_EVE_IMAG_O, 8 * span * 2, 8 * span, times, img);
+    SB_DMA_WRITE(P_compute_ODD_REAL_O, 8 * span * 2, 8 * span, times, real + span);
+    SB_DMA_WRITE(P_compute_ODD_IMAG_O, 8 * span * 2, 8 * span, times, img + span);
+
     SB_WAIT_ALL();
   }
 }
