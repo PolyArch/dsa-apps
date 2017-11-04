193315bdfe3d9c720a2cb74e75594c5c3775504c
Jian Weng <jian.weng@ucla.edu>
2017-11-03 19:45:35 -0700

    2-vectorized fft done, but some weird result is given

--- benchmarks/new-fft/softbrain.cc
+++ benchmarks/new-fft/softbrain.cc
@@ -2,7 +2,9 @@
 #include <cmath>
 #include <algorithm>
 #include "sb_insts.h"
-#include "compute.h"
+#include "sim_timing.h"
+#include "compute0.h"
+#include "compute1.h"
 
 #define complex_mul(a, b) (a).real() * (b).real() - (a).imag() * (b).imag(), \
   (a).real() * (b).imag() + (a).imag() * (b).real()
@@ -21,26 +23,34 @@ complex<float> _buffer[N];
 #define PI 3.14159265358979303
 
 void fft(complex<float> *_a, complex<float> *w) {
-  SB_CONFIG(compute_config, compute_size);
+  SB_CONFIG(compute0_config, compute0_size);
+
   complex<float> *from = _a, *to = _buffer;
-  for (int blocks = N / 2; blocks; blocks >>= 1) {
-    int span = N / blocks;
-    SB_DMA_READ(from,          2 * blocks * 8, blocks * 8, span / 2, P_compute_L);
-    SB_DMA_READ(from + blocks, 2 * blocks * 8, blocks * 8, span / 2, P_compute_R);
+  int span = 2;
+  for (int blocks = N / 2; blocks != 1; blocks >>= 1, span <<= 1) {
+    SB_DMA_READ(from,          2 * blocks * 8, blocks * 8, span / 2, P_compute0_L);
+    SB_DMA_READ(from + blocks, 2 * blocks * 8, blocks * 8, span / 2, P_compute0_R);
 
-    SB_REPEAT_PORT(blocks);
-    SB_DMA_READ(w, 8 * blocks, 8, span / 2, P_compute_W);
-    //for (int j = 0; j < span / 2; ++j) {
-      //SB_DMA_READ(w + j * blocks, 0, 8, blocks, P_compute_W);
-    //}
+    SB_REPEAT_PORT(blocks / 2);
+    SB_DMA_READ(w, 8 * blocks, 8, span / 2, P_compute0_W);
 
-    SB_DMA_WRITE(P_compute_A, 8, 8, N / 2, to);
-    SB_DMA_WRITE(P_compute_B, 8, 8, N / 2, to + N / 2);
+    SB_DMA_WRITE(P_compute0_A, 8, 8, N / 2, to);
+    SB_DMA_WRITE(P_compute0_B, 8, 8, N / 2, to + N / 2);
 
     SB_WAIT_ALL();
     swap(from, to);
   }
-  if (from != _a) {
+
+  SB_CONFIG(compute1_config, compute1_size);
+  SB_DMA_READ(from,     16, 8, N / 2, P_compute1_L)
+  SB_DMA_READ(from + 1, 16, 8, N / 2, P_compute1_R)
+  SB_DMA_READ(w, 8, 8, N / 2, P_compute1_W);
+  SB_DMA_WRITE(P_compute1_A, 8, 8, N / 2, to);
+  SB_DMA_WRITE(P_compute1_B, 8, 8, N / 2, to + N / 2);
+  SB_WAIT_ALL();
+  //swap(from, to);
+
+  if (to != _a) {
     for (int i = 0; i < N; ++i) {
       _a[i] = _buffer[i];
     }
