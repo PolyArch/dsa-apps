efeb1711c0547b8709af600fc4668c0ab82f4af7
Tony Nowatzki <tony.nowatzki@gmail.com>
2017-11-08 22:45:32 -0800

    add forgotten test

---
+++ func-test/multi_simple.c
@@ -0,0 +1,37 @@
+#include "testing.h"
+#include "add1.h"
+
+static DTYPE add1[ASIZE]; 
+static DTYPE answer_add1[ASIZE]; 
+
+static DTYPE out2[ASIZE]; 
+
+
+int main(int argc, char* argv[]) {
+  init();
+
+  for(int i=0; i < ASIZE; ++i) {
+    answer_add1[i]=i+1;
+  }
+
+  begin_roi();
+
+  SB_CONTEXT(0x0001);
+  SB_CONFIG(add1_config,add1_size);
+  SB_DMA_READ(&in[0],ABYTES,ABYTES,1,P_add1_in);
+  SB_DMA_WRITE(P_add1_out,8,8,AWORDS,&out2[0]);
+
+  SB_CONTEXT(0x0002);
+  SB_CONFIG(none_config,none_size);
+  SB_DMA_READ(&in[0],ABYTES,ABYTES,1,P_none_in);
+  SB_DMA_WRITE(P_none_out,8,8,AWORDS,&out[0]);
+
+  SB_CONTEXT(0x0003);
+  SB_WAIT_ALL();
+
+  end_roi();
+
+  compare<DTYPE>(argv[0],out2,answer_add1,ASIZE);
+  compare<DTYPE>(argv[0],out,in,ASIZE);
+
+}
