f8320b0c338a61c8852a10d156bf5d921c628309
Tony Nowatzki <tony.nowatzki@gmail.com>
2017-11-09 21:24:08 -0800

    added a new interface for the *stretch* parameter, the difference in the block length per iteration

--- common/include/sb_insts.h
+++ common/include/sb_insts.h
@@ -12,43 +12,55 @@
 #define SB_CONFIG(mem_addr, size) \
   __asm__ __volatile__("sb_cfg  %0, %1" : : "r"(mem_addr), "i"(size));
 
-
 //Fill the scratchpad from DMA (from memory or cache)
 //Note that scratch_addr will be written linearly
-#define SB_DMA_SCRATCH_LOAD(mem_addr, stride, access_size, num_strides, scratch_addr) \
-  __asm__ __volatile__("sb_stride    %0, %1" : : "r"(stride), "r"(access_size)); \
+#define SB_DMA_SCRATCH_LOAD_STRETCH(mem_addr, stride, acc_size, stretch, n_strides, scr_addr) \
+  __asm__ __volatile__("sb_stride    %0, %1, %2" : : "r"(stride), "r"(acc_size), "i"(stretch)); \
   __asm__ __volatile__("sb_dma_addr  %0, %1" : : "r"(mem_addr), "r"(mem_addr)); \
-  __asm__ __volatile__("sb_dma_scr   %0, %1, 0" : : "r"(num_strides), "r"(scratch_addr));
+  __asm__ __volatile__("sb_dma_scr   %0, %1, 0" : : "r"(n_strides), "r"(scr_addr));
+
+//Maintain compatibility with old thing (stretch=0)
+#define SB_DMA_SCRATCH_LOAD(mem_addr, stride, acc_size, n_strides, scr_addr) \
+  SB_DMA_SCRATCH_LOAD_STRETCH(mem_addr,stride, acc_size, 0, n_strides, scr_addr)
+
 
 //Fill the scratchpad from DMA (from memory or cache)
 //Note that mem_addr will be written linearly
 #define SB_SCRATCH_DMA_STORE(scr_addr, stride, access_size, num_strides, mem_addr) \
-  __asm__ __volatile__("sb_stride    %0, %1" : : "r"(stride), "r"(access_size)); \
+  __asm__ __volatile__("sb_stride    %0, %1, 0" : : "r"(stride), "r"(access_size)); \
   __asm__ __volatile__("sb_dma_addr  %0, %1" : : "r"(mem_addr), "r"(mem_addr)); \
   __asm__ __volatile__("sb_scr_dma   %0, %1, 0" : : "r"(num_strides), "r"(scr_addr));
 
 
 //Read from scratch into a cgra port
-#define SB_SCR_PORT_STREAM(scr_addr, stride, access_size, num_strides, port ) \
-  __asm__ __volatile__("sb_stride   %0, %1" : : "r"(stride), "r"(access_size)); \
+#define SB_SCR_PORT_STREAM_STRETCH(scr_addr,stride,acc_size,stretch,n_strides, port) \
+  __asm__ __volatile__("sb_stride %0, %1, %2" : : "r"(stride), "r"(acc_size), "i"(stretch)); \
   __asm__ __volatile__("sb_scr_rd   %0, %1, %2 " : : "r"(scr_addr), "r"(num_strides), "i"(port)); 
+
+#define SB_SCR_PORT_STREAM(scr_addr,stride,acc_size,n_strides, port) \
+   SB_SCR_PORT_STREAM_STRETCH(scr_addr,stride,acc_size,0,n_strides, port) 
+
 
 //A convienience CMD if you want to read linearly
 #define SB_SCRATCH_READ(scr_addr, num_bytes, port) \
-  __asm__ __volatile__("sb_stride   %0, %1" : : "r"(8), "r"(8)); \
+  __asm__ __volatile__("sb_stride   %0, %1, 0" : : "r"(8), "r"(8)); \
   __asm__ __volatile__("sb_scr_rd   %0, %1, %2 " : : "r"(scr_addr), "r"(num_bytes/8), "i"(port)); 
 
 //Read from DMA into a port
-#define SB_DMA_READ(mem_addr, stride, access_size, num_strides, port ) \
-  __asm__ __volatile__("sb_stride    %0, %1" : : "r"(stride), "r"(access_size)); \
-  __asm__ __volatile__("sb_dma_rd    %0, %1, %2" : : "r"(mem_addr), "r"(num_strides), "i"(port)); 
+#define SB_DMA_READ_STRETCH(mem_addr, stride, acc_size, stretch, n_strides, port ) \
+  __asm__ __volatile__("sb_stride %0, %1, %2" : : "r"(stride), "r"(acc_size), "i"(stretch)); \
+  __asm__ __volatile__("sb_dma_rd %0, %1, %2" : : "r"(mem_addr), "r"(n_strides), "i"(port)); 
+
+#define SB_DMA_READ(mem_addr, stride, acc_size, n_strides, port ) \
+  SB_DMA_READ_STRETCH(mem_addr, stride, acc_size, 0, n_strides, port )
+
 
 #define SB_DMA_READ_SIMP(mem_addr, num_strides, port ) \
   __asm__ __volatile__("sb_dma_rd    %0, %1, %2" : : "r"(mem_addr), "r"(num_strides), "i"(port)); 
 
 //Throw away some outputs.  We will add a proper instruction for this at some point, rather then writing to memory
 #define SB_GARBAGE(output_port, num_elem) \
-  __asm__ __volatile__("sb_stride   %0, %1" : : "r"(8), "r"(8)); \
+  __asm__ __volatile__("sb_stride   %0, %1, 0" : : "r"(8), "r"(8)); \
   __asm__ __volatile__("sb_wr_dma   %0, %1, %2"   : : "r"(0), "r"(num_elem), "i"(output_port|0x100)); 
 
 //Throw away some outputs.  We will add a proper instruction for this at some point, rather then writing to memory
@@ -64,12 +76,16 @@
 
 // Plain Write to Memory
 #define SB_DMA_WRITE(output_port, stride, access_size, num_strides, mem_addr) \
-  __asm__ __volatile__("sb_stride   %0, %1" : : "r"(stride), "r"(access_size)); \
+  __asm__ __volatile__("sb_stride   %0, %1, 0" : : "r"(stride), "r"(access_size)); \
   __asm__ __volatile__("sb_wr_dma   %0, %1, %2"   : : "r"(mem_addr), "r"(num_strides), "i"(output_port)); 
 
 // This is for optimizations when core is a bottleneck, break into two commands:
+#define SB_STRIDE_STRETCH(stride, acc_size, stretch) \
+ __asm__ __volatile__("sb_stride %0, %1, %2" : : "r"(stride), "r"(acc_size), "i"(stretch));
+
 #define SB_STRIDE(stride, access_size) \
-  __asm__ __volatile__("sb_stride   %0, %1" : : "r"(stride), "r"(access_size)); 
+   SB_STRIDE_STRETCH(stride, access_size, 0)
+
 
 #define SB_DMA_WRITE_SIMP(output_port, num_strides, mem_addr) \
   __asm__ __volatile__("sb_wr_dma   %0, %1, %2"   : : "r"(mem_addr), "r"(num_strides), "i"(output_port)); 
@@ -77,12 +93,12 @@
 
 //Write to DMA, but throw away all but the last 16-bits from each word
 #define SB_DMA_WRITE_SHF16(output_port, stride, access_size, num_strides, mem_addr) \
-  __asm__ __volatile__("sb_stride   %0, %1" : : "r"(stride), "r"(access_size)); \
+  __asm__ __volatile__("sb_stride   %0, %1, 0" : : "r"(stride), "r"(access_size)); \
   __asm__ __volatile__("sb_wr_dma   %0, %1, %2"   : : "r"(mem_addr),  "r"(num_strides), "i"(output_port|0x40)); 
 
 //Write to DMA, but throw away all but the last 32-bits from each word  (implemented, not tested yet)
 #define SB_DMA_WRITE_SHF32(output_port, stride, access_size, num_strides, mem_addr) \
-  __asm__ __volatile__("sb_stride   %0, %1" : : "r"(stride), "r"(access_size)); \
+  __asm__ __volatile__("sb_stride   %0, %1, 0" : : "r"(stride), "r"(access_size)); \
   __asm__ __volatile__("sb_wr_dma   %0, %1, %2"   : : "r"(mem_addr),  "r"(num_strides), "i"(output_port|0x80)); 
 
 
