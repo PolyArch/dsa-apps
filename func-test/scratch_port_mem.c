61b19c783cf9ac8ee6354f2569f32c2c02922c19
Tony Nowatzki <tony.nowatzki@gmail.com>
2017-12-12 12:25:43 -0800

    minor bench fix

--- func-test/scratch_port_mem.c
+++ func-test/scratch_port_mem.c
@@ -1,8 +1,11 @@
 #include "testing.h"
 
 int main(int argc, char* argv[]) {
+  init();
+
   begin_roi();
   SB_CONFIG(none_config,none_size);
+  SB_WAIT_ALL();
   SB_SCRATCH_READ(0, ABYTES, P_none_in);
   SB_DMA_WRITE(P_none_out,8,8,AWORDS,&out[0]);
   SB_WAIT_ALL();
