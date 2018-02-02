099eee1d6925407d21ee5ac583e07b04a41b8719
Tony Nowatzki <tony.nowatzki@gmail.com>
2018-02-01 19:21:41 -0800

    fix discard fill test 2

---
+++ func-test/fix_discard_fill2.c
@@ -0,0 +1,33 @@
+#include "testing.h"
+#include "add1_vec.h"
+
+#define N 5
+#define M 3
+#define TOTAL ((N) * (M))
+
+uint64_t input[TOTAL];
+uint64_t output[TOTAL];
+uint64_t answer[TOTAL];
+
+
+int main(int argc, char* argv[]) {
+  init();
+  for (int i = 0; i < TOTAL; ++i) {
+    input[i] = i;
+    answer[i] = i + 1;
+  }
+
+  SB_FILL_MODE(STRIDE_DISCARD_FILL);
+
+  begin_roi();
+  SB_CONFIG(add1_vec_config,add1_vec_size);
+
+  SB_DMA_READ(input, M * 8, M * 8, N, P_add1_vec_in);
+  SB_DMA_WRITE(P_add1_vec_out, M * 8, M * 8, N, output);
+
+
+  SB_WAIT_ALL();
+  end_roi();
+
+  compare<uint64_t>(argv[0], answer, output, TOTAL);
+}
