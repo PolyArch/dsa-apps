2052d21e7668b89878c29410947968d9840da9e8
Tony Nowatzki <tony.nowatzki@gmail.com>
2018-02-02 13:27:01 -0800

    fixed discard test

--- func-test/fix_discard_fill.c
+++ func-test/fix_discard_fill.c
@@ -18,22 +18,22 @@ int main(int argc, char* argv[]) {
 
   answer[0]=2;  //every fourth input was 0
   answer[1]=3;
-  answer[2]=4;
-  answer[3]=5;
-  answer[4]=6;
-  answer[5]=7;
-  answer[6]=2;
-  answer[7]=3;
+  answer[2]=6;
+  answer[3]=7;
+  answer[4]=2;
+  answer[5]=3;
+  answer[6]=4;
+  answer[7]=5;
 
   SB_CONFIG(add1_vec_o2_config,add1_vec_o2_size);
   SB_FILL_MODE(STRIDE_DISCARD_FILL);
 
   begin_roi();
-  SB_DMA_READ(&input[0],   8, 8, 6, P_add1_vec_o2_in);
-  SB_DMA_READ(&input[0],   8, 8, 2, P_add1_vec_o2_in);
+  SB_DMA_READ(&input[0],   8, 8*6, 1, P_add1_vec_o2_in);
+  SB_DMA_READ(&input[0],   8, 8*2, 1, P_add1_vec_o2_in);
 
-  SB_DMA_WRITE(P_add1_vec_o2_outA,8,8, 8,&output[0]);
-  //SB_DMA_WRITE(P_add1_vec_o2_outB,8,8, 2,&output[4]);
+  SB_DMA_WRITE(P_add1_vec_o2_outA,8,8, 6,&output[0]);
+  SB_DMA_WRITE(P_add1_vec_o2_outB,8,8, 2,&output[6]);
 
   SB_WAIT_ALL();
   end_roi();
