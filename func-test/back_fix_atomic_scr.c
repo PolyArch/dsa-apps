8187e20005329ed381fdfeceb13dacf688e74c9f
vidushidadu <vdadu@g.ucla.edu>
2018-05-28 16:49:15 -0700

    Edited back_fix_atomic_scr test code

--- func-test/back_fix_atomic_scr.c
+++ func-test/back_fix_atomic_scr.c
@@ -9,11 +9,8 @@
 
 int main() {
 
-  uint64_t ind[N/2];
-  for(uint64_t i=0; i<N/2; ++i) {
-    ind[i] = i*2;
-  }
   uint64_t x[N];
+  // uint64_t a[N]; uint64_t b[N];
   for(uint64_t i=0; i<N; ++i) {
     x[i] = i;
   }
@@ -21,14 +18,11 @@ int main() {
   begin_roi();
   SB_CONFIG(add_offset_config,add_offset_size);
   
-  SB_CONST_SCR(0, 0, (64*2+2));
-  SB_WAIT_SCR_WR();
-  
-  SB_DMA_READ(&ind[0], 8, 8, N/2, P_IND_1);
-  SB_CONFIG_INDIRECT1(T64, T64, 1, 1);
-  SB_INDIRECT(P_IND_1, &x[0], N/2, P_add_offset_A);
+  SB_DMA_READ(&x[0], 8, 8, N, P_add_offset_A);
   
   SB_CONST(P_add_offset_const, 1, N/2);
+  // SB_DMA_WRITE(P_add_offset_C, 8, 8, N, &a[0]);
+  // SB_DMA_WRITE(P_add_offset_D, 8, 8, N, &b[0]);
   SB_ATOMIC_SCR_OP(P_add_offset_C, P_add_offset_D, 0, N, 0);
   SB_WAIT_SCR_WR();
   SB_WAIT_ALL();
