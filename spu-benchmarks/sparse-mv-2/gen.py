c6e1d6ded16d77146f058a6580d648aa487ba291
Jian Weng <jian.weng@ucla.edu>
2018-06-20 14:27:35 -0700

    x86 code work

--- spu-benchmarks/sparse-mv-2/gen.py
+++ spu-benchmarks/sparse-mv-2/gen.py
@@ -5,8 +5,6 @@ try:
 except:
     n, m, s0, s1 = int(sys.argv[1]), int(sys.argv[2]), float(sys.argv[3]), 1.0
 
-a = numpy.random.randn(n, m)
-
 def compress_line(a, n, ratio):
     last = 0
     line = []
@@ -17,16 +15,22 @@ def compress_line(a, n, ratio):
                 last += 2 ** 4 - 1
             line.append('%d %f' % (j - last, a[j]))
             last = j
+        else:
+            a[j] = 0.0
     if len(line) % 2:
         line.append('1 0.0')
     line.append(('-1 -1'))
     return line
 
-for i in range(n):
-    print ' '.join(compress_line(a[i], m, s0))
-
-print
-
+a = numpy.random.randn(n, m)
 v = numpy.random.randn(m)
-print ' '.join(compress_line(v, m, s1))
 
+with open('input.data', 'w') as f_input:
+    for i in range(n):
+        f_input.write(' '.join(compress_line(a[i], m, s0)) + '\n')
+    f_input.write(' '.join(compress_line(v, m, s1)))
+
+
+with open('output.data', 'w') as f_output:
+    ref = numpy.dot(a, v)
+    f_output.writelines('\n'.join('%.3f' % i for i in ref))
--- spu-benchmarks/sparse-mv-2/main.cc
+++ spu-benchmarks/sparse-mv-2/main.cc
@@ -1,15 +1,24 @@
 #include <cstdio>
 #include <cstdint>
 #include <cassert>
+#include <cmath>
+#include <cstdlib>
 #include "../../common/include/sim_timing.h"
 
 #ifndef FxPnt
-#define FxPnt 10
+#define FxPnt 8
 #endif
 
 #define INT16MAX ((1<<16)-1)
 
 #define fx_to_flt(x) ((float)(x) / (1<<FxPnt))
+#define flt_to_fx(x) ((int)((x) * (1<<FxPnt)))
+
+#ifdef DEBUG
+#define fx_mul(a, b) flt_to_fx((fx_to_flt(a) * fx_to_flt(b)))
+#else
+#define fx_mul(a, b) (a) * (b)
+#endif
 
 struct Sparse {
   uint16_t delta;
@@ -26,35 +35,47 @@ void dump(int n, const Sparse *a) {
 
 #ifdef __x86_64__
 
-void spmv(int n_a, const Sparse *a, int n_b, const Sparse *b, uint16_t *c) {
+void spmv(int n_a, const Sparse *a, int n_b, const Sparse *b, int16_t *c) {
   //printf("%d %d\n", (int)a[0].delta, (int)a[0].val);
-  for (int i = 0, ja = 0; i < N; ++i) {
-    int jb, idx_a, idx_b;
-    for (jb = 0, idx_a = a[ja].delta, idx_b = b[jb].delta;
+  for (int i = 0, ja = 1; i < N; ++i) {
+    for (int jb = 1, idx_a = a[ja - 1].delta + a[ja].delta, idx_b = b[0].delta + b[1].delta;
           a[ja].delta != INT16MAX && b[jb].delta != INT16MAX; ) {
+      if (idx_a - a[ja].delta == idx_b) {
+        c[i] += fx_mul(a[ja - 1].val, b[jb].val);
+      } else if (idx_b - b[jb].delta == idx_a) {
+        c[i] += fx_mul(b[jb - 1].val, a[ja].val);
+      } else {
+        if (idx_a == idx_b) {
+          c[i] += fx_mul(b[jb].val, a[ja].val);
+        }
+        if (idx_a - a[ja].delta == idx_b - b[jb].delta) {
+          c[i] += fx_mul(a[ja - 1].val, b[jb - 1].val);
+        }
+      }
       if (idx_a < idx_b) {
+        idx_a += a[++ja].delta;
         idx_a += a[++ja].delta;
       } else if (idx_b < idx_a) {
         idx_b += b[++jb].delta;
+        idx_b += b[++jb].delta;
       } else {
-        c[i] += a[ja].val * b[jb].val;
         idx_a += a[++ja].delta;
+        idx_a += a[++ja].delta;
+        idx_b += b[++jb].delta;
         idx_b += b[++jb].delta;
       }
     }
     //printf("%d %d %d %d\n", ja, (int) a[ja].delta, jb, (int) b[jb].delta);
     while(a[ja].delta != INT16MAX)
-      ++ja;
-    assert (a[ja].delta == INT16MAX);
-    ++ja;
-    assert(a[ja].delta == INT16MAX);
-    ++ja;
-    if (i == N - 1) {
-      //printf("%d %d %d\n", i, ja, n_a);
-      assert(ja == n_a);
-    } else {
-      //printf("%d %d\n", i, ja);
+      ja += 2;
+#ifdef DEBUG
+    //printf("row %d done\n", i);
+    assert (a[ja - 1].delta == INT16MAX);
+    if (i != N - 1) {
+      assert(a[ja + 1].delta != INT16MAX);
     }
+#endif
+    ja += 2;
   }
 }
 
@@ -63,11 +84,11 @@ void spmv(int n_a, const Sparse *a, int n_b, const Sparse *b, uint16_t *c) {
 #endif
 
 Sparse a[(int)(N * M * S0 * 1.5)], b[M + 10];
-uint16_t c[N];
+int16_t c[N];
+int total_a = 0, total_b = 0;
 
-int main() {
+void input_data() {
   FILE* input = fopen("input.data", "r");
-  int total_a = 0;
 #define total total_a
   for (int i = 0; i < N; ++i) {
     int delta;
@@ -87,7 +108,6 @@ int main() {
   }
 #undef total
 
-  int total_b = 0;
 #define total total_b
   while (true) {
     int delta;
@@ -102,12 +122,36 @@ int main() {
     }
   }
 #undef total
-  
-  //dump(total_a, a);
+}
+
+void check_correctness() {
+#ifdef DEBUG
+  FILE *output = fopen("output.data", "r");
+  for (int i = 0; i < N; ++i) {
+    float _c = fx_to_flt(c[i]);
+    float ref;
+    assert(fscanf(output, "%f", &ref) == 1);
+    if(fabs(_c - ref) > 1) {
+      printf("expected %f but %f @%d\n", ref, _c, i);
+      //assert(false);
+    }
+  }
+#else
+  printf("Non-debug mode, correctness check skipped!\n");
+#endif
+}
+
+int main() {
+  input_data();
+
+#ifndef DEBUG
   spmv(total_a, a, total_b, b, c);
+#endif
   begin_roi();
   spmv(total_a, a, total_b, b, c);
   end_roi();
+
+  check_correctness();
 
   return 0;
 }
