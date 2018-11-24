ceb8bdf536c18cdd3a2dbf098034fdf15f75e88d
vidushidadu <vdadu@g.ucla.edu>
2018-11-24 10:38:04 -0800

    simplifed scnn re-sparsify code

--- final_spu_benchmarks/datasets_multicore/scnn/main.cpp
+++ final_spu_benchmarks/datasets_multicore/scnn/main.cpp
@@ -9,6 +9,7 @@
 #include "/home/vidushi/ss-stack/ss-workloads/common/include/net_util_func.h"
 #include <inttypes.h>
 #include <assert.h>
+#include <math.h>
 #define NUM_THREADS	2
 
 using namespace std;
@@ -97,79 +98,90 @@ vector<uint16_t> neuron_i_val[(Nx*Ny)/(Tx*Ty)][Ni];
 vector<uint16_t> neuron_i_ind[(Nx*Ny)/(Tx*Ty)][Ni];
 uint16_t neuron_i_ptr[(Nx*Ny*Ni)/(Tx*Ty)];
 
-// sparse output activations
-vector<uint16_t> neuron_o_val[(Nx*Ny)/(Tx*Ty)][Nn];
-vector<uint16_t> neuron_o_ind[(Nx*Ny)/(Tx*Ty)][Nn];
+// sparse output activations (TODO: allocate space for memory for now)
+// vector<uint16_t> neuron_o_val[(Nx*Ny)/(Tx*Ty)][Nn];
+// vector<uint16_t> neuron_o_ind[(Nx*Ny)/(Tx*Ty)][Nn];
+uint16_t neuron_o_val[(Nx*Ny)/(Tx*Ty)][Nn][Tx*Ty];
+uint16_t neuron_o_ind[(Nx*Ny)/(Tx*Ty)][Nn][Tx*Ty];
+
 uint16_t neuron_o_ptr[(Nx*Ny*Nn)/(Tx*Ty)];
 
-
-// TODO:FIXME: Check if we should include Ni variation inside the kernel
+// working on act of neuron_i_val[z][y]
+// working on weight of synapse_val[y][x]
 void kernel(int x, int y, int z) {
 
-  // working on act of neuron_i_val[z][y]
-  // working on weight of synapse_val[y][x]
-
   unsigned size_neuron_tile = neuron_i_val[z][y].size();
+  // not sure why ceil is not working
+  unsigned ceiled_sn_tile = ceil(size_neuron_tile/float(8));
+  // ceiled_sn_tile = ceiled_sn_tile*8;
+
+  if(size_neuron_tile%8!=0) {
+	ceiled_sn_tile = size_neuron_tile+8-size_neuron_tile%8;
+  } else {
+	ceiled_sn_tile = size_neuron_tile;
+  }
+  int pad_size = ceiled_sn_tile-size_neuron_tile;
+
   unsigned size_synapse = synapse_val[y][x].size();
-  int num_comp_inst = size_synapse*size_neuron_tile; // number of 16-bit instances required
+
+
+  // although this should be taken care in the simulator
+  if(size_synapse==0 || size_neuron_tile==0)
+	return;
+
+  int num_comp_inst = size_synapse*ceiled_sn_tile;
+  cout << size_neuron_tile << " " << ceiled_sn_tile << " " << size_synapse << " " << num_comp_inst << endl;
 
   SB_CONST_SCR(0, 0, Tx*Ty*Tn);
   SB_WAIT_ALL();
-  // SB_WAIT_SCR_WR();
 
   SB_CONFIG(scnn_config,scnn_size);
 
+  // frequency of synapses
+  SB_REPEAT_PORT(ceiled_sn_tile/8);
+  SB_DMA_READ(&synapse_val[y][x][0], 2, 2, size_synapse, P_scnn_sval);
+
+  SB_REPEAT_PORT(ceiled_sn_tile/8);
+  SB_DMA_READ(&synapse_ind[y][x][0], 2, 2, size_synapse, P_scnn_sind);
+ 
+  SB_CONST(P_scnn_ky, Ky, num_comp_inst/8);
+  SB_CONST(P_scnn_ty, Ty, num_comp_inst/8);
+  SB_CONST(P_scnn_const, Ty-Ky+1, num_comp_inst/8);
+
+  // frequency of neurons
+  for(unsigned is=0; is<size_synapse; ++is) {
+    SB_DMA_READ(&neuron_i_val[z][y][0], 2, 2, size_neuron_tile, P_scnn_nval);
+    SB_DMA_READ(&neuron_i_ind[z][y][0], 2, 2, size_neuron_tile, P_scnn_nind);
+	SB_CONST(P_scnn_nval, 0, pad_size); // nothing should happen here
+	SB_CONST(P_scnn_nind, 1, pad_size);
+  }
+  SB_CONFIG_ATOMIC_SCR_OP(T16, T16, T32);
+  SB_ATOMIC_SCR_OP(P_scnn_A, P_scnn_B, 0, num_comp_inst, 0);
+
   // re-sparsification code
-  // TODO: should read from banked scratchpad
-  // FIXME: check the initial value
   SB_DMA_READ(&out_n[x*Tn][z*(Kx*Ky)], 2, 2, num_inputs, P_scnn_neuron);
-  SB_CONST(P_scnn_constt, fused_const, num_inputs);
-  SB_CONST(P_scnn_dummy, 1, num_inputs);
-  // TODO: should write to? maybe memory here (but ideally?)
-  // Write to banked scratchpad addresses otherwise malloc required (overwrite
-  // over the previous one because result cannot be generated unless it is read
-  // -- so only 2 partitions should be fine)
+  SB_CONST(P_scnn_num_in, num_inputs, num_inputs);
+  SB_CONST(P_scnn_acc_ctrl, 0, num_inputs);
+
+  SB_STRIDE(2,2);
   SB_DMA_WRITE_SIMP(P_scnn_inval, num_inputs*2, &neuron_o_val[z][x*Tn][0]);
   SB_DMA_WRITE_SIMP(P_scnn_inind, num_inputs*2, &neuron_o_ind[z][x*Tn][0]);
 
-  // convolution code -- TODO: should be coming from banked scratchpad
-  SB_REPEAT_PORT(size_neuron_tile);
-  SB_DMA_READ(&synapse_val[y][x][0], sizeof(uint16_t), sizeof(uint16_t), size_synapse, P_scnn_sval);
-
-  SB_REPEAT_PORT(size_neuron_tile);
-  SB_DMA_READ(&synapse_ind[y][x][0], sizeof(uint16_t), sizeof(uint16_t), size_synapse, P_scnn_sind);
-
-  // printf("size_synapse: %d size_neuron: %d num_comp_inst: %d\n",size_synapse,size_neuron_tile, num_comp_inst);
-  
-  // TODO: should be coming from linear scratchpad
-  for(unsigned is=0; is<size_synapse; ++is) {
-    SB_DMA_READ(&neuron_i_val[x][y][0], sizeof(uint16_t), sizeof(uint16_t), neuron_i_val[x][y].size(), P_scnn_nval);
-    SB_DMA_READ(&neuron_i_ind[x][y][0], sizeof(uint16_t), sizeof(uint16_t), neuron_i_ind[x][y].size(), P_scnn_nval);
-  }
- 
-  SB_CONST(P_scnn_Ky, Ky, num_comp_inst);
-  SB_CONST(P_scnn_Ty, Ty, num_comp_inst);
-  SB_CONST(P_scnn_const, Ty-Ky+1, num_comp_inst);
-
-  SB_CONFIG_ATOMIC_SCR_OP(T16, T16, T16);
-  // SB_CONFIG_ATOMIC_SCR_OP(T16, T32, T16);
-  SB_ATOMIC_SCR_OP(P_scnn_C, P_scnn_D, 0, num_comp_inst, 0);
-
-  //----------------------------
   SB_WAIT_SCR_ATOMIC();
   uint64_t done;
   SB_RECV(P_scnn_done, done);
   SB_RESET();
+
+
   SB_WAIT_ALL();
 }
-
 /*
 void send_halos() {
   // this is important to know this address
-  // TODO: make scr_scr_port also 8-bit wide
   SB_SCR_REM_SCR(0, 8, 8, (Tx*Ty*4)*2/8, 0, 0);
 }
 */
+
 
 void convolution_layer_blocked(long tid) {
   // wait on the halos
@@ -210,7 +222,9 @@ void *entry_point(void *threadid) {
 
 void read_weights() {
 
-  FILE *weight_val_file = fopen("datasets/wgt_val.data","r"); 
+  string str(wgt_val_file);
+  FILE *weight_val_file = fopen(str.c_str(),"r"); 
+  // FILE *weight_val_file = fopen("datasets/wgt_val.data","r"); 
   char lineToRead[5000];
   
   while(fgets(lineToRead, 5000, weight_val_file)!=NULL){
@@ -227,7 +241,9 @@ void read_weights() {
   }
   fclose(weight_val_file);
 
-  FILE *weight_ind_file = fopen("datasets/wgt_index.data","r"); 
+  str = wgt_ind_file;
+  FILE *weight_ind_file = fopen(str.c_str(),"r"); 
+  // FILE *weight_ind_file = fopen("datasets/wgt_index.data","r"); 
   
   while(fgets(lineToRead, 5000, weight_ind_file)!=NULL){
 	std::string raw(lineToRead);
@@ -244,7 +260,9 @@ void read_weights() {
   }
   fclose(weight_ind_file);
 
-  FILE *weight_ptr_file = fopen("datasets/wgt_ptr.data","r"); 
+  str = wgt_ptr_file;
+  FILE *weight_ptr_file = fopen(str.c_str(),"r"); 
+  // FILE *weight_ptr_file = fopen("datasets/wgt_ptr.data","r"); 
   
   while(fgets(lineToRead, 5000, weight_ptr_file)!=NULL){
 	std::string raw(lineToRead);
@@ -261,7 +279,9 @@ void read_activations() {
 
   int dim1 = (Nx*Ny)/(Tx*Ty);
 
-  FILE *act_val = fopen("datasets/act_val.data","r"); 
+  string str(act_val_file);
+  FILE *act_val = fopen(str.c_str(),"r"); 
+  // FILE *act_val = fopen("datasets/act_val.data","r"); 
   char lineToRead[5000];
 
   while(fgets(lineToRead, 5000, act_val)!=NULL){
@@ -278,7 +298,9 @@ void read_activations() {
   }
   fclose(act_val);
 
-  FILE *act_ind = fopen("datasets/act_index.data","r"); 
+  str = act_ind_file;
+  FILE *act_ind = fopen(str.c_str(),"r"); 
+  // FILE *act_ind = fopen("datasets/act_index.data","r"); 
   
   while(fgets(lineToRead, 5000, act_ind)!=NULL){
 	std::string raw(lineToRead);
@@ -294,7 +316,9 @@ void read_activations() {
   }
   fclose(act_ind);
 
-  FILE *act_ptr = fopen("datasets/act_ptr.data","r"); 
+  str = act_ptr_file;
+  FILE *act_ptr = fopen(str.c_str(),"r"); 
+  // FILE *act_ptr = fopen("datasets/act_ptr.data","r"); 
   
   while(fgets(lineToRead, 5000, act_ptr)!=NULL){
 	std::string raw(lineToRead);
